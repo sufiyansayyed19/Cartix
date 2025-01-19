fa2b7ea81d81e361c0be06f1707805a68dacc65f
Sayyed Sufiyan <sufiyan.sayed92@gmail.com>
2025-01-19 22:18:41 +0530

    in userController status value are removed, pls refere note written note

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,7 +11,7 @@ const createToken = (id) => {
 
 
 
-
+//NOTE: .status(value) is reomved due to complications in grabing error message and success status in frontend.
 
 //Route for user Login
 const loginUser = async (req, res) => {
@@ -19,28 +19,28 @@ const loginUser = async (req, res) => {
         const {email, password} = req.body;
 
         if (!email || !password) {
-            return res.status(400).json({ success: false, message: "Please provide both email and password" });
+            return res.json({ success: false, message: "Please provide both email and password" });
         }
 
         const user = await userModel.findOne({email});
 
         if (!user) {
-            return res.status(400).json({success: false, message: "Invalid credentials"});
+            return res.json({success: false, message: "Invalid credentials"});
         }
         
         const passwordMatch = await bcrypt.compare(password, user.password);
         
         if (!passwordMatch){
-            return res.status(400).json({success: false, message: "Invalid credentials"});
+            return res.json({success: false, message: "Invalid credentials"});
         }
 
         const token = createToken(user.id);
-        return res.status(200).json({ success:true, token})
+        return res.json({ success:true, token})
 
 
     } catch (error){
         console.log("Error: ", error);
-        res.status(500).json({ success:false, Message: error.message});
+        res.json({ success:false, message: "Invalid credentials"});
     }
 }
 
@@ -54,23 +54,23 @@ const registerUser = async (req, res) => {
 
         const userExist = await userModel.findOne({email});
         if (userExist){
-            return res.status(400).json({success: false, message: "User already exists"});
+            return res.json({success: false, message: "User already exists"});
         }
 
         // validation email & password
         if (!validator.isEmail(email)) {
-            return res.status(400).json({success: false, message: "Please enter a valid email"});
+            return res.json({success: false, message: "Please enter a valid email"});
         }
 
         if (password.length < 8) {
-            return res.status(400).json({success:false, message: "At least 8 characters passwrod are required"});
+            return res.json({success:false, message: "At least 8 characters passwrod are required"});
         }
 
         // salt to hash user password
-        const salt = await bycrypt.genSalt(10);
+        const salt = await bcrypt.genSalt(10);
 
         // hash password
-        const hashedPassword = await bycrypt.hash(password, salt);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         // creating new user
         const newUser = new userModel ({
@@ -83,12 +83,12 @@ const registerUser = async (req, res) => {
 
         const token = createToken(user._id);
         
-        return res.status(201).json({ success:true, token})
+        return res.json({ success:true, token})
         
 
     } catch(error){
         console.log("Error: ", error);
-        res.status(500).json({ success:false, Message: error.message});
+        res.json({ success:false, message: error.message});
     }
 
 
@@ -104,13 +104,13 @@ const adminLogin = async (req, res) => {
             const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '2d' });
 
             // console.log('Token generated')
-            return res.status(200).json({success: true, token});
+            return res.json({success: true, token});
         } else {
-            return res.status(401).json({ success: false, Message: "Invalid credentials" });
+            return res.json({ success: false, message: "Invalid credentials" });
         }
     } catch (error) {
         console.log("Error: ", error);
-        res.status(500).json({ success:false, Message: error.message});
+        res.json({ success:false, message: error.message});
     }
 
 
