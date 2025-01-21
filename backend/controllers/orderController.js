5a74afa9da4c164df4ab0b426d995e0260ed683a
Sayyed Sufiyan <sufiyan.sayed92@gmail.com>
2025-01-21 22:10:13 +0530

    orderModel created and controller initialized

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,27 @@
+
+// Placing orders using Stripe Method
+const placeOrderStripe = async (req, res)=> {
+
+}
+
+// Placing orders using Razorpay Method
+const placeOrderRazorpay = async (req, res)=> {
+
+}
+
+// All orders data for Admin Panel
+const allOrders = async (req, res)=> {
+
+}
+
+// User Order Data for Frontend
+const userOrders = async (req, res)=> {
+
+}
+
+// update order status from Admin
+const updateStatus = async (req, res)=> {
+
+}
+
+export { placeOrderStripe, placeOrderRazorpay, allOrders, userOrders, updateStatus}
