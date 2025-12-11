4a2b23c34950a3947193a598a1c5e1a5de8ce2ac
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-12-11 19:35:34 +0530

    login.jsx updated

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,20 +1,18 @@
-import React, { useState } from 'react'
+import PropTypes from "prop-types";
+import { useState } from 'react'
 import { backendUrl } from '../App'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
-const Login = ({setToken}) => {
-
+const Login = ({ setToken }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const onSubmitHandler = async (e) => {
         try {
             e.preventDefault();
-    
-            // Sending login request
             const response = await axios.post(`${backendUrl}/api/user/admin`, { email, password });
-    
+
             if (response.data.success) {
                 setToken(response.data.token);
             } else {
@@ -26,30 +24,47 @@ const Login = ({setToken}) => {
             toast.error(message);
         }
     };
-    
 
+    return (
+        <div className='flex items-center justify-center min-h-screen w-full'>
+            <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
+                <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+                <form>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
+                        <input 
+                            onChange={(e)=> setEmail(e.target.value)} 
+                            className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' 
+                            type="email" 
+                            required 
+                        />
+                    </div>
 
-  return (
-    <div className='flex items-center justify-center min-h-screen w-full'>
-        <div className='bg-white shadow-md rounded-lg px-8 py-6 max-w-md'>
-            <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
-            <form>
-                <div className='mb-3 min-w-72'>
-                    <p className='text-sm font-medium text-gray-700 mb-2'>Email Address</p>
-                    <input onChange={(e)=> setEmail(e.target.value)} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="email" placeholder='your@email.com' required />
-                </div>
+                    <div className='mb-3 min-w-72'>
+                        <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
+                        <input 
+                            onChange={(e)=> setPassword(e.target.value)} 
+                            className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' 
+                            type="password" 
+                            required 
+                        />
+                    </div>
 
-                <div className='mb-3 min-w-72'>
-                    <p className='text-sm font-medium text-gray-700 mb-2'>Password</p>
-                    <input onChange={(e)=> setPassword(e.target.value)} className='rounded-md w-full px-3 py-2 border border-gray-300 outline-none' type="password" placeholder='your@email.com' required />
-                </div>
-
-                <button onClick={onSubmitHandler} className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black active:text-black active:bg-white' type='submit'> Login </button>
-                
-            </form>
+                    <button 
+                        onClick={onSubmitHandler} 
+                        className='mt-2 w-full py-2 px-4 rounded-md text-white bg-black active:text-black active:bg-white' 
+                        type='submit'
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
-export default Login
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired,
+};
+
+export default Login;
