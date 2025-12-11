7e5eddf10a33e1a41686b81b3d2f3129182072e9
sufiyansayyed19 <sufiyan.sayed92@gmail.com>
2025-12-11 19:59:07 +0530

    backend/models/userModel.js updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,8 +4,10 @@ const userSchema = new mongoose.Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    phone: {type: String, default: ''},
+    address: {type: String, default: ''},
     cartData: {type: Object, default: {}},
-}, {minimize:false});
+}, {minimize:false, timestamps: true});
 
 const userModel = mongoose.model("user", userSchema); // Register again with updated schema
 
